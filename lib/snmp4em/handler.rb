a6143f760ef147656f32f69eafa26f66760dfb69
Michael Shapiro <koudelka@ryoukai.org>
2010-05-19 18:38:05 -0400

    Changing Handler to a class, for testing, and adding some notes.

--- lib/snmp4em/handler.rb
+++ lib/snmp4em/handler.rb
@@ -1,15 +1,22 @@
 module SNMP4EM
-  module Handler #:nodoc:
+  class Handler < EventMachine::Connection #:nodoc:
     def receive_data(data)
       begin
         message = SNMP::Message.decode(data)
       rescue Exception => err
+        # the request that this malformed response corresponds to
+        # will timeout and retry
         return
       end
       
       response = message.pdu
       request = Manager.pending_requests[response.request_id]
 
+      #
+      # in the event of a timeout retry, the request will have been 
+      # pruned from the Manager, so the response is to an expired
+      # request, ignore it.
+      #
       request.handle_response(response) if request
     end
   end
